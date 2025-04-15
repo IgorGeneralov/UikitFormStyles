3fff014022c114fa440c58f47c33936c53421bd2
Igor Generalov <igor@generalov.net>
2025-04-15 13:57:45 +0300

    Update uikitformstyles.php

--- uikitformstyles.php
+++ uikitformstyles.php
@@ -11,10 +11,24 @@ class PlgSystemUikitFormStyles extends CMSPlugin
 	public function onBeforeCompileHead()
 	{
 		$app = Factory::getApplication();
+
 		if ($app->isClient('site')) {
+			$params = $this->params;
 			$document = Factory::getDocument();
-			$pluginPath = URI::base() . 'plugins/system/uikitformstyles/uikitformstyles.js';
-			$document->addScript($pluginPath);
+			$baseUri = URI::base() . 'plugins/system/uikitformstyles/js/';
+
+			$scriptMap = [
+				'enable_forms' => 'forms.js',
+				'enable_buttons' => 'buttons.js',
+				'enable_modal' => 'modal.js',
+				'enable_text' => 'text.js',
+			];
+
+			foreach ($scriptMap as $paramName => $scriptFile) {
+				if ($params->get($paramName, 1) && file_exists(JPATH_ROOT . '/plugins/system/uikitformstyles/js/' . $scriptFile)) {
+					$document->addScript($baseUri . $scriptFile);
+				}
+			}
 		}
 	}
 }
