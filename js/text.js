6aaba6b2bed63cd224fb4d069e413bf798caaec1
Igor Generalov <igor@generalov.net>
2025-04-15 15:23:25 +0300

    Update text.js

--- js/text.js
+++ js/text.js
@@ -3,4 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
 		el.classList.remove('small', 'text-muted');
 		el.classList.add('uk-text-meta', 'uk-margin-small-bottom');
 	});
+
+	document.querySelectorAll('.form-control-feedback').forEach(function (el) {
+		el.classList.remove('form-control-feedback');
+		el.classList.add('uk-text-small', 'uk-text-danger');
+	});
 });
